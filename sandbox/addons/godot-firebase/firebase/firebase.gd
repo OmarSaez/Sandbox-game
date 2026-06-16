9223bdddd525d91d4e446e1abc8574459564ba4c
OmarSaez <omar.saez@usach.cl>
2026-06-16 12:23:49 -0400

    Se mueve las variables para poder exportar en el apk

--- sandbox/addons/godot-firebase/firebase/firebase.gd
+++ sandbox/addons/godot-firebase/firebase/firebase.gd
@@ -105,7 +105,9 @@ func _check_emulating() -> void:
 func _load_config() -> void:
 	if not (_config.apiKey != "" and _config.authDomain != ""):
 		var env = ConfigFile.new()
-		var err = env.load("res://addons/godot-firebase/.env")
+		var err = env.load("res://env.cfg")
+		if err != OK:
+			err = env.load("res://addons/godot-firebase/.env")
 		if err == OK:
 			for key in _config.keys():
 				var config_value = _config[key]
